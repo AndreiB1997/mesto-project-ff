846d9471aea9443a42c940c891421420ba3c402d
Andrei B <msnksj@yandex.ru>
2024-06-06 13:56:53 +0500

    Проблемы с функцией удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+//создание и вывод шаблона-карточки
+function createCard(link, name) {
+  const placesList = document.querySelector('.places__list');
+  const cardTemplate = document.querySelector('#card-template').content;
+  
+  const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  placesItem.querySelector('.card__image').src = link;
+  placesItem.querySelector('.card__image').alt = name;
+  placesItem.querySelector('.card__title').textContent = name;
 
-// @todo: Вывести карточки на страницу
+  const cardDeleteButton = placesItem.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', cardDelete(placesItem))
+
+  placesList.append(placesItem);
+
+}
+
+//наполнение и вывод всех карточек из массива
+initialCards.forEach(function(card){
+  createCard(card.link, card.name)
+})
+
+function cardDelete(card) {
+  card.remove()
+}
+
+
+
+
+
+
